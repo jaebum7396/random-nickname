1a1e5b3b042959493bc53ae63f166312098f0ffd
jhdl0157 <jhdl0157@naver.com>
2022-02-17 13:24:18 +0900

    Update

--- src/main/java/com/example/gitaction/TestController.java
+++ src/main/java/com/example/gitaction/TestController.java
@@ -1,15 +1,19 @@
 package com.example.gitaction;
 
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${custom.myname}")
+    private String name;
+
     @GetMapping("/") public String hello()
     {
-
-        return "Update Cashing V2!!!!!!";
+        return "Hello"+name;
     }
 }
 
