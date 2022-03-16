e5c844c8856e9ad28e1b5a0b04587f7c9da3fba6
jhdl0157 <jhdl0157@naver.com>
2022-03-16 13:22:19 +0900

    Add : defaultValue

--- src/main/java/com/example/gitaction/TestController.java
+++ src/main/java/com/example/gitaction/TestController.java
@@ -5,6 +5,7 @@ package com.example.gitaction;
 import com.example.gitaction.service.NicknameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +20,8 @@ import java.util.List;
 public class TestController {
     private final NicknameService nicknameService;
 
-    @GetMapping("/nickname") public List<String> getnickname(@RequestParam(value = "count",required = false) int count)
+    @GetMapping("/nickname") public List<String> getnickname(@RequestParam(defaultValue = "2")  int count)
     {
-        if(count==0) count=2;
         return  nicknameService.makeNickname(count);
     }
 }
--- src/main/java/com/example/gitaction/service/NicknameService.java
+++ src/main/java/com/example/gitaction/service/NicknameService.java
@@ -10,8 +10,8 @@ import static java.util.Collections.shuffle;
 
 @Service
 public class NicknameService {
-      String noun[] = new String[]{"사과","대추","아이패드","대통령","마우스","대벌레"};
-      String adjective[] = new String[]{"맛있는","멋진","사악한","매운","떫은","군침싹도는","마약한"};
+      String noun[] = new String[]{"사과","대추","아이패드","대통령","마우스","대벌레","고양이","제이지","로션","탱크"};
+      String adjective[] = new String[]{"맛있는","멋진","사악한","매운","떫은","군침싹도는","마약한","민망한","표독스러운"};
     public List<String> makeNickname(int count){
         List<String> nsa = new ArrayList<>();
         for(int i=0;i<count;i++){
