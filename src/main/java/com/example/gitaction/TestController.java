7248542a768778d52875ddfdd2418fe67addb392
jhdl0157 <jhdl0157@naver.com>
2022-03-16 12:59:32 +0900

    Add : 랜덤닉네임

--- src/main/java/com/example/gitaction/TestController.java
+++ src/main/java/com/example/gitaction/TestController.java
@@ -2,18 +2,27 @@ package com.example.gitaction;
 
 
 
+import com.example.gitaction.service.NicknameService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TestController {
-    @Value("${custom.myname}")
-    private String name;
+import javax.xml.ws.Response;
+import java.util.List;
 
-    @GetMapping("/") public String hello()
+@RestController
+@RequestMapping(path = "/api")
+@RequiredArgsConstructor
+public class TestController {
+    private final NicknameService nicknameService;
+
+    @GetMapping("/nickname") public List<String> getnickname(@RequestParam(value = "count",required = false) int count)
     {
-        return "Hello"+name;
+        if(count==0) count=2;
+        return  nicknameService.makeNickname(count);
     }
 }
 
---
+++ src/main/java/com/example/gitaction/service/NicknameService.java
@@ -0,0 +1,25 @@
+package com.example.gitaction.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.shuffle;
+
+@Service
+public class NicknameService {
+      String noun[] = new String[]{"사과","대추","아이패드","대통령","마우스","대벌레"};
+      String adjective[] = new String[]{"맛있는","멋진","사악한","매운","떫은","군침싹도는","마약한"};
+    public List<String> makeNickname(int count){
+        List<String> nsa = new ArrayList<>();
+        for(int i=0;i<count;i++){
+            shuffle(Arrays.asList(noun));
+            shuffle(Arrays.asList(adjective));
+            nsa.add(adjective[i]+" "+noun[i]);
+        }
+        return nsa;
+    }
+
+}
