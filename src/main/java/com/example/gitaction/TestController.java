5a680d660d3d737cedbdf6c7da65360986be70f1
주재범 <jaebum7396@naver.com>
2024-10-29 09:29:54 +0900

    init. https://github.com/jhdl0157/githubAction-Random-Nickname 에서 포크함

--- src/main/java/com/example/gitaction/TestController.java
+++ src/main/java/com/example/gitaction/TestController.java
@@ -1,24 +1,18 @@
 package com.example.gitaction;
 
 
-
 import com.example.gitaction.common.ListResult;
 import com.example.gitaction.common.service.ResponseService;
 import com.example.gitaction.service.NicknameService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.IIOException;
-import javax.xml.ws.Response;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1")
