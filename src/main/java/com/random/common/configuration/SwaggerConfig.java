7652c826b4928c84cab296a28927e4446557d94a
주재범 <jaebum7396@naver.com>
2024-10-29 11:20:42 +0900

    refactor. 기본값1

--- src/main/java/com/random/common/configuration/SwaggerConfig.java
+++ src/main/java/com/random/common/configuration/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.random"))
+            .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build()
             .securityContexts(Arrays.asList(securityContext())) // 추가
